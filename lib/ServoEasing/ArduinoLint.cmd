4713d7436f639885cbca56b4b3847994c966f140
i3creations <michael.crandall@i3creations.com>
2023-03-31 13:00:38 -0600

    cleanup

--- lib/ServoEasing/ArduinoLint.cmd
+++
@@ -1,6 +0,0 @@
-if exist keywords.txt  (
-  E:\Elektro\arduino_common\arduino-lint.exe --library-manager update --compliance strict
-) else (
-  echo "Please change directory to library root"
-)
-pause
--- lib/ServoEasing/Doxygen.cmd
+++
@@ -1,6 +0,0 @@
-if exist keywords.txt  (
-  E:\Elektro\doxygen\bin\doxygen.exe
-) else (
-  echo "Please change directory to library root"
-)
-pause
